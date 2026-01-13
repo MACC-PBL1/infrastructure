abe027fb3b61c54131bf3f75ae052251a6f30c35
oierfernandezg <oier.fernandezg@alumni.mondragon.edu>
2026-01-13 13:38:46 +0100

    implementacion de system manager en vpc2 y vpc1

--- terraform_modular_vpc1/terraform/modules/rds/variables.tf
+++ terraform_modular_vpc1/terraform/modules/rds/variables.tf
@@ -3,8 +3,5 @@ variable "private_subnet_ids" { type = list(string) }
 variable "rds_sg_id" { type = string }
 variable "db_name" { type = string }
 variable "db_master_username" { type = string }
-variable "db_master_password" {
-  type      = string
-  sensitive = true
-}
+
 variable "db_instance_class" { type = string }
